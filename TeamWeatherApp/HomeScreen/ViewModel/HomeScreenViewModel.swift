c59ad5ee67812c2b6d374fc1c650903ad629c84b
myCRObaM <matej.hetzel@gmail.com>
2019-09-24 12:37:08 +0200

    Alamofire Request done

--- TeamWeatherApp/HomeScreen/ViewModel/HomeScreenViewModel.swift
+++ TeamWeatherApp/HomeScreen/ViewModel/HomeScreenViewModel.swift
@@ -7,24 +7,66 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 
 class HomeScreenViewModel: ViewModelType   {
+    
+    init(dependencies: HomeScreenViewModel.Dependencies){
+        self.dependencies = dependencies
+    }
+    
     func transform(input: HomeScreenViewModel.Input) -> HomeScreenViewModel.Output {
-        return Output()
+        var disposables = [Disposable]()
+        self.input = input
+        
+        disposables.append(getData(subject: input.getDataSubject))
+        
+        self.output = Output(dataIsReadySubject: PublishSubject(), locationIsMissingSubject: PublishSubject(), disposables: disposables)
+        
+        return output
     }
     
     struct Input {
-        
+        var requestSettingsSubject: ReplaySubject<Bool>
+        var getDataSubject: ReplaySubject<Bool>
     }
     
     struct Output {
-        
+        var dataIsReadySubject: PublishSubject<Bool>
+        var locationIsMissingSubject: PublishSubject<Bool>
+        var disposables: [Disposable]
     }
     
     struct Dependencies {
-        
+        var alamofireRepository: AlamofireRepository
+        var scheduler: SchedulerType
     }
     
+    var dependencies: Dependencies
+    var input: Input!
+    var output: Output!
+    var mainWeatherData: MainWeatherClass?
+    
+    let units: String = "si"
+    let location: String = "45.82176,17.39763"
+    
+    
+    func getData(subject: ReplaySubject<Bool>) -> Disposable  {
+        return subject
+            .flatMap({[unowned self] bool -> Observable<MainWeatherClass> in
+                let af = AlamofireRepository()
+                return af.alamofireRequest(self.units, self.location)
+            })
+            .observeOn(MainScheduler.instance)
+            .subscribeOn(dependencies.scheduler)
+            .subscribe(onNext: {[unowned self]  bool in
+                self.output.dataIsReadySubject.onNext(true)
+                self.mainWeatherData = bool
+            }
+        )
+        
+    }
 
 }
---
+++ TeamWeatherAppTests/HomeScreenViewModelTest.swift
@@ -0,0 +1,70 @@
+//
+//  HomeViewModelTest.swift
+//  TeamWeatherAppTests
+//
+//  Created by Matej Hetzel on 24/09/2019.
+//  Copyright © 2019 Josip Marković. All rights reserved.
+//
+
+import XCTest
+import RxTest
+import RxSwift
+import Nimble
+import Quick
+import Cuckoo
+@testable import TeamWeatherApp
+
+
+class HomeScreenViewModelTest: QuickSpec {
+    override func spec() {
+        describe("setup Data"){
+            var mainWeatherData: MainWeatherClass!
+            let mockedAlamofireRepo = MockAlamofireRepository()
+            var testScheduler: TestScheduler!
+            var homeScreenViewModel: HomeScreenViewModel!
+            let disposeBag = DisposeBag()
+            beforeSuite {
+                Cuckoo.stub(mockedAlamofireRepo) { mock in
+                    let testBundle = Bundle(for: HomeScreenViewModelTest.self)
+                    guard let path = testBundle.url(forResource: "MainWeatherData", withExtension: "json") else {return}
+                    let dataFromLocation = try! Data(contentsOf: path)
+                    let weather = try! JSONDecoder().decode(MainWeatherClass.self, from: dataFromLocation)
+                    when(mock.alamofireRequest(any(), any())).thenReturn(Observable.just(weather))
+                    
+                    mainWeatherData = weather
+                }
+            }
+            context("Initialize HomeScreenViewModel"){
+                var dataReadySubject: TestableObserver<Bool>!
+                beforeEach {
+                    testScheduler = TestScheduler(initialClock: 0)
+                    
+                    homeScreenViewModel = HomeScreenViewModel(dependencies: HomeScreenViewModel.Dependencies(alamofireRepository: AlamofireRepository(), scheduler: ConcurrentDispatchQueueScheduler(qos: .background)))
+                    
+                    let output = homeScreenViewModel.transform(input: HomeScreenViewModel.Input(requestSettingsSubject: ReplaySubject<Bool>.create(bufferSize: 1), getDataSubject: ReplaySubject<Bool>.create(bufferSize: 1)))
+                    
+                    for disposable in output.disposables {
+                        disposable.disposed(by: disposeBag)
+                    }
+                    
+                    dataReadySubject = testScheduler.createObserver(Bool.self)
+                    homeScreenViewModel.output.dataIsReadySubject.subscribe(dataReadySubject).disposed(by: disposeBag)
+                    
+                }
+                it("Check if dataReady subject is triggered"){
+                    testScheduler.start()
+                    
+                    homeScreenViewModel.input.getDataSubject.onNext(true)
+                expect(dataReadySubject.events.count).toEventually(equal(1))
+                }
+                it("Check if viewModel is getting correct Data"){
+                    testScheduler.start()
+                    homeScreenViewModel.input.getDataSubject.onNext(true)
+
+                    expect(homeScreenViewModel.mainWeatherData?.daily.data.count).toEventually(equal(mainWeatherData.daily.data.count))
+
+                }
+            }
+        }
+    }
+}
