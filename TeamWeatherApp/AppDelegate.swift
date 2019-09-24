4c5233d5d99fbf81752742be3678f2d595c84bee
Josip Marković <josip.markovic@factory.hr>
2019-09-24 09:53:12 +0200

    Project initialization

---
+++ TeamWeatherApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TeamWeatherApp
+//
+//  Created by Josip Marković on 24/09/2019.
+//  Copyright © 2019 Josip Marković. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ TeamWeatherApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TeamWeatherApp
+//
+//  Created by Josip Marković on 24/09/2019.
+//  Copyright © 2019 Josip Marković. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
