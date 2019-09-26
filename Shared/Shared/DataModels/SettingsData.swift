91d9fb9a370b74c90c1a9f6a8a487861248c22c3
Josip Marković <josip.markovic@factory.hr>
2019-09-26 13:20:18 +0200

    Fixed data models placement

--- Shared/Shared/DataModels/SettingsData.swift
+++ Shared/Shared/DataModels/SettingsData.swift
@@ -1,13 +1,13 @@
 //
 //  SettingsData.swift
-//  WeatherAppSettings
+//  Shared
 //
 //  Created by Josip Marković on 26/09/2019.
 //  Copyright © 2019 Josip Marković. All rights reserved.
 //
 
 import Foundation
-import Shared
+
 
 public struct SettingsData{
     
