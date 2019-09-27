217ddd97a0fc18edcdee83017e8c5c052f56bdaf
myCRObaM <matej.hetzel@gmail.com>
2019-09-27 07:37:56 +0200

    added font

--- WeatherAppSearch/WeatherAppSearchTests/GeneratedMocks.swift
+++ WeatherAppSearch/WeatherAppSearchTests/GeneratedMocks.swift
@@ -1,4 +1,4 @@
-// MARK: - Mocks generated from file: WeatherAppSearch/Networking/Repository/LocationRepository.swift at 2019-09-26 13:44:22 +0000
+// MARK: - Mocks generated from file: WeatherAppSearch/Networking/Repository/LocationRepository.swift at 2019-09-27 05:36:47 +0000
 
 //
 //  LocationRepository.swift
