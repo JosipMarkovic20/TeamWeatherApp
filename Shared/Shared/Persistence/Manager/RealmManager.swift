de59ac804a8178e2d66ae298e02592fd51bb47d4
Josip Marković <josip.markovic@factory.hr>
2019-09-27 07:42:58 +0200

    Added MARK comments

--- Shared/Shared/Persistence/Manager/RealmManager.swift
+++ Shared/Shared/Persistence/Manager/RealmManager.swift
@@ -15,7 +15,7 @@ class RealmManager {
     
     let realm = try? Realm()
     
-    
+    //MARK: Delete location
     func deleteLocation(geonameId: Int) -> Observable<String>{
         guard let realmLocation = self.realm?.object(ofType: RealmLocation.self, forPrimaryKey: geonameId) else { return Observable.just("Object not found!")}
         
@@ -29,7 +29,7 @@ class RealmManager {
         return Observable.just("Object deleted!")
     }
     
-    //Saving specific location to realm
+    //MARK: Save location
     func saveLocation(location: Locations) -> Observable<String>{
         let realmLocation = RealmLocation()
         realmLocation.createRealmLocation(location: location)
@@ -43,7 +43,7 @@ class RealmManager {
         return Observable.just("Object saved!")
     }
     
-    //Saving last viewed location to realm
+    //MARK: Save last location
     func saveLastLocation(location: Locations) -> Observable<String>{
         let realmLocation = LastRealmLocation()
         realmLocation.createLastRealmLocation(location: location)
@@ -57,7 +57,7 @@ class RealmManager {
         return Observable.just("Object saved!")
     }
     
-    //Deleting last viewd location from realm
+    //MARK: Delete last location
     func deleteLastLocation() -> Observable<String>{
         let backThreadRealm = try? Realm()
         guard let allLastLocations = backThreadRealm?.objects(LastRealmLocation.self) else { return Observable.just("Object not found!") }
@@ -71,20 +71,20 @@ class RealmManager {
         return Observable.just("Settings deleted!")
     }
     
-    //Fetching last viewed location from realm
+    //MARK: Get last location
     func getLastLocation() -> Results<LastRealmLocation>{
         let backThreadRealm = try! Realm()
         return backThreadRealm.objects(LastRealmLocation.self)
     }
     
-    //Fetching all previous locations from realm
+    //MARK: Get all locations
     func getLocations() -> Results<RealmLocation>{
         let backThreadRealm = try! Realm()
         return backThreadRealm.objects(RealmLocation.self)
     }
     
     
-    //Deleting settings objects from realm
+    //MARK: Delete settings
     func deleteSettings() -> Observable<String>{
         let backThreadRealm = try? Realm()
         guard let allSettingsObjects = backThreadRealm?.objects(RealmSettings.self) else { return Observable.just("Object not found!") }
@@ -99,7 +99,7 @@ class RealmManager {
     }
     
     
-    //Saving settings object to realm
+    //MARK: Save settings
     func saveSettings(settings: SettingsData) -> Observable<String>{
         let backThreadRealm = try? Realm()
         let settingsToSave = RealmSettings()
@@ -115,7 +115,7 @@ class RealmManager {
     }
     
     
-    //Fetching settings object from realm
+    //MARK: Get settings
     func getSettings() -> Results<RealmSettings>{
         let backThreadRealm = try! Realm()
         return backThreadRealm.objects(RealmSettings.self)
